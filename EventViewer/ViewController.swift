4a47c21205ac686b52810bb9969870b041ef83fb
Alaattin Bedir <alaattinbedir@hotmail.com>
2018-08-09 11:02:00 +0300

    Initial Commit

---
+++ EventViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventViewer
+//
+//  Created by Alaattin Bedir on 9.08.2018.
+//  Copyright © 2018 Magiclamp Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
